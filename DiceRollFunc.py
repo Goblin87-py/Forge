0d5daa74274e0f079c2afc131b6836ad225fdd16
Goblin87-py <marcusgodden87@outlook.com>
2025-01-12 16:58:31 +0000

    dice roll function

---
+++ DiceRollFunc.py
@@ -0,0 +1,74 @@
+import random
+
+def dice_roll():
+  while True:  
+    press_enter = "Press Enter to roll the dice: > "
+    user_input = input(press_enter)
+    roll = random.randint(1, 6)
+    
+    if roll == 1:
+        print('''
+             _________
+            |         |
+            |         |
+            |    O    |
+            |         |
+            |_____ ___|
+              
+              ''')
+
+    if roll == 2:
+        print('''
+             _________
+            |         |
+            |    O    |
+            |         |
+            |    O    |
+            |_____ ___|
+              
+              ''')
+
+    if roll == 3:
+        print('''
+             _________
+            |         |
+            |  O      |
+            |    O    |
+            |      O  |
+            |_________|
+              
+              ''')
+        
+    if roll == 4:
+        print('''
+             _________
+            |         |
+            |  O   O  |
+            |         |
+            |  O   O  |
+            |_____ ___|
+              ''')
+ 
+    if roll == 5:
+        print('''
+             _________
+            |         |
+            |  O   O  |
+            |    O    |
+            |  O   O  |
+            |_____ ___|
+              
+              ''')
+
+    if roll == 6:
+        print('''
+             _________
+            |         |
+            |  O   O  |
+            |  O   O  |
+            |  O   O  |
+            |_____ ___|
+              
+              ''')
+
+dice_roll()
