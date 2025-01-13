153a1cc2cac28abeedf3d2bfd880917981a62dfe
Goblin87-py <marcusgodden87@outlook.com>
2025-01-13 14:49:13 +0000

    charcter update

--- char(frame).py
+++ char(frame).py
@@ -1,62 +1,68 @@
+
 import random
 
+class Character(object):
 
-
-
-
-
-class user():
-
-    def __init__(self, health=100, stamina=50): #these are attributes
+    def __init__(self, health=10, stamina=10):
         self.health = health
         self.stamina = stamina
-        print(f"{health}, {stamina}")
-        if self.stamina >= 0:
-            print("You're out of Stamina")
+    
+    def display_status(self, name = "Character"):
+        print(f''' Name: {name}
+ Health: {self.health}
+ Stamina: {self.stamina}''')
         
-        dead = self.health == 0
+         
+                
+class CharacterCombatActions(Character):
+
+    def __init__(self, health=10, stamina=10):
+        super().__init__(health, stamina)
        
     def parry_action(self, cost_parry=5):
         self.stamina -= cost_parry
-        print('''Roll 1d6, If the result is an even number, You parry the next atack and gain 5 stamina.
-             If the result is an odd number, Miss the parry and loose 5 stamina''') 
-    
+        
         result = random.randint(1, 6)
-        print(result)
-        if result: 
-            2, 4 or 6 
+
+        if result in (2, 4, 6):
+            print(f"You rolled:", {result})
             print("You Parried!")
             self.stamina += 5
-        else: 
-            print("You missed!")
+         
+            
+        if result in (1, 3, 5):
+            print(f"You rolled:", {result})
+            print("You Missed!")
             self.stamina -= 5
 
+        self.display_status()  
+            
+    
     def block_action(self, cost_block=5):
         self.stamina -= cost_block
-        print("Roll 1d6 to block")
 
         result = random.randint(1, 6)
-     
-        print(result)
-
-        if result == (3, 4, 5 or 6):
-            print("You blocked")
+        
+        if result in (3, 4, 5, 6):
+            print(f"You rolled: {result}")
+            print("You blocked!") 
         else:
-            print("You missed the block")
+            print(f"You rolled: {result}")
+            print("You missed the block!")
 
-    def attack_action(self, cost_atk=5):
+
+    def atk_action(self, cost_atk=5):
         self.stamina -= cost_atk
-        print("Roll 1d6 to Attack")
-
+        
         result = random.randint(1, 6)
 
-        print(result)
-
-        if result == (3, 4, 5 or 6):
-            print("You hit!")
-        else: 
-            print("You missed")
-
+        if result in (3, 4, 5, 6):
+            print(f"You rolled: {result}")
+            print("You Hit!")    
+        
+        if result in (1, 2):
+            print(f"You rolled: {result}") 
+            print("You Missed!")
 
 
 
